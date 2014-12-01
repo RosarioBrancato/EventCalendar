ede7ab52732e855e6de859522f3f10bd49673d45
Rosario B. <rossa_4057@hotmail.com>
2014-12-01 21:53:15 +0100

    Log Out Correction

--- domain/LogOut.php
+++ domain/LogOut.php
@@ -5,13 +5,20 @@
 	}
 	
 	include_once('../constant/Constants.php');
+	include_once('../bo/MessageBO.php');
+	include_once('../gui/LogInGUI.php');
 	
 	if(isset($_POST['log_out']) && (session_status() == PHP_SESSION_ACTIVE)) {
+		//destroy session
+		session_unset();
 		session_destroy();
+		//show login gui
+		showLogInGui(null, new MessageBO('Du wurdest erfolgreich abgemeldet.', MESSAGE_TYPE_SUCCESS));
+		
+	} else {
+		//redirect
+		header('Location: ' . URL . 'domain/LogIn.php');
 	}
-	
-	//redirect
-	header('Location: ' . URL . 'domain/LogIn.php');
 	exit;
 	
 ?>
